fc0f189b1cc33d78a824a8474f0d20ac050e6925
Benoît <benoit.maillard@epfl.ch>
2025-02-15 01:29:00 +0800

    Reorganize waypoints code

--- src/app/map-view/map.service.ts
+++ src/app/map-view/map.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, signal } from '@angular/core';
-import { BehaviorSubject, catchError, EMPTY, filter, Observable, Subject, throwError, zip } from 'rxjs';
+import { BehaviorSubject, catchError, EMPTY, filter, Observable, Subject, throwError, zip, map } from 'rxjs';
 import { GraphHopperService } from '../graphhopper/graphhopper.service';
 import { RoutePath } from '../graphhopper/route-path.model';
 import { Point } from './point.model';
@@ -11,100 +11,94 @@ export class MapService {
   public zoomLevel: number = 11;
   public center: [number, number] = [7.17, 46.64];
   public waypoints = signal<Point[]>([]);
-  public paths = signal<RoutePath[]>([])
+  public paths = signal<RoutePath[]>([]);
   private error = new Subject<string>();
   public errorStream = this.error.asObservable();
 
-  constructor(
-    private graphhopperService: GraphHopperService
-  ) { }
+  constructor(private graphhopperService: GraphHopperService) { }
+
+  private updateWaypoints(index: number, replacement: Point) {
+    this.waypoints.update(old => [
+      ...old.slice(0, index),
+      replacement,
+      ...old.slice(index + 1)
+    ]);
+  }
+
+  private updatePaths(index: number, newPaths: RoutePath[]) {
+    this.paths.update(old => [
+      ...old.slice(0, Math.max(0, index - 1)),
+      ...newPaths,
+      ...old.slice(index + 1)
+    ]);
+  }
 
   addWayPoint(waypoint: Point) {
-    // TODO we should also probably update the point to the snapped point
-    if (this.waypoints().length > 0) {
-      this.computePath(this.waypoints().at(-1), waypoint).subscribe(
-        newPath => {
-          this.paths.update(old => [...old, newPath]);
-          this.waypoints.update(old => [...old, waypoint]);
-        }
-      )
-    } else {
-      // we have to make a request to make sure this is a valid position
-      this.computePath(waypoint, waypoint).subscribe(
-        _ => this.waypoints.update(old => [...old, waypoint])
-      )
-    }
+    const waypoints = this.waypoints();
+    const lastWaypoint = waypoints.at(-1);
+    const path$ = lastWaypoint
+      ? this.computePath(lastWaypoint, waypoint).pipe(map(path => [path]))
+      : this.computePath(waypoint, waypoint).pipe(map(() => []));
+
+    path$.subscribe(newPaths => {
+      this.paths.update(old => [...old, ...newPaths]);
+      this.waypoints.update(old => [...old, waypoint]);
+    });
   }
 
   removeWayPoint(index: number) {
-    const oldLength = this.waypoints().length;
-    this.waypoints.update(old => [...old.slice(0, index), ...old.slice(index + 1, old.length)]);
+    const waypoints = this.waypoints();
+    this.waypoints.update(old => [...old.slice(0, index), ...old.slice(index + 1)]);
 
-    if (index == 0) {
-      this.paths.update(old => old.slice(1, old.length));
-    } else if (index == oldLength - 1) {
-      this.paths.update(old => old.slice(0, old.length - 1));
+    if (index === 0) {
+      this.paths.update(old => old.slice(1));
+    } else if (index === waypoints.length - 1) {
+      this.paths.update(old => old.slice(0, -1));
     } else {
-      // TODO we should probably have a mechanism to avoid inconsistencies after an API call (lock? index table? linkedlist?)
-      this.computePath(this.waypoints()[index - 1], this.waypoints()[index]).subscribe(
-        newPath => this.paths.update(old => [...old.slice(0, index - 1), newPath, ...old.slice(index + 1, old.length)])
-      )
+      this.computePath(waypoints[index - 1], waypoints[index])
+        .subscribe(newPath => this.updatePaths(index, [newPath]));
     }
   }
 
   moveWayPoint(replacement: Point, index: number) {
+    // we need this to make sure that the marker gets updated properly
     const saveLat = this.waypoints()[index].lat;
     const saveLon = this.waypoints()[index].lon;
     this.waypoints()[index].lat = replacement.lat;
     this.waypoints()[index].lon = replacement.lon;
 
+    const waypoints = this.waypoints();
+    let path$: Observable<any>;
+
     if (index == 0) {
-      this.computePath(replacement, this.waypoints()[index + 1])
-        .subscribe({
-          next: newPath => {
-            this.paths.update(old => [newPath, ...old.slice(index + 1, old.length)]);
-            this.waypoints.update(old => [...old.slice(0, index), replacement, ...old.slice(index + 1, old.length)]);
-          },
-          error: err => {
-            this.waypoints()[index].lat = saveLat
-            this.waypoints()[index].lon = saveLon
-          }
-        });
-    } else if (index == this.waypoints().length - 1) {
-      this.computePath(this.waypoints()[index - 1], replacement)
-        .subscribe({
-          next: newPath => {
-            this.paths.update(old => [...old.slice(0, index - 1), newPath]);
-            this.waypoints.update(old => [...old.slice(0, index), replacement, ...old.slice(index + 1, old.length)]);
-          },
-          error: err => {
-            this.waypoints()[index].lat = saveLat
-            this.waypoints()[index].lon = saveLon
-          }
-        });
+      path$ = this.computePath(replacement, waypoints[index + 1]).pipe(map(path => [path]));
+    } else if (index == waypoints.length - 1) {
+      path$ = this.computePath(waypoints[index - 1], replacement).pipe(map(path => [path]));
     } else {
-      zip(
-        this.computePath(this.waypoints()[index - 1], replacement),
-        this.computePath(replacement, this.waypoints()[index + 1])
-      ).subscribe({
-        next: newPaths => {
-          this.paths.update(old => [...old.slice(0, index - 1), ...newPaths, ...old.slice(index + 1, old.length)]);
-          this.waypoints.update(old => [...old.slice(0, index), replacement, ...old.slice(index + 1, old.length)]);
-        },
-        error: err => {
-          this.waypoints()[index].lat = saveLat
-          this.waypoints()[index].lon = saveLon
-        }
-      });
+      path$ = zip(
+        this.computePath(waypoints[index - 1], replacement),
+        this.computePath(replacement, waypoints[index + 1])
+      );
     }
+
+    path$.subscribe({
+      next: newPaths => {
+        this.updatePaths(index, newPaths);
+        this.updateWaypoints(index, replacement);
+      },
+      error: () => {
+        this.waypoints()[index].lat = saveLat;
+        this.waypoints()[index].lon = saveLon;
+      }
+    });
   }
 
   computePath(from: Point, target: Point): Observable<RoutePath> {
     return this.graphhopperService.computePath([from, target]).pipe(
       catchError(error => {
-        console.log("Bad point!!") // TODO replace with an error message
+        console.log("Bad point!!"); // TODO replace with an error message
         return throwError(() => error);
       })
-    )
+    );
   }
 }
